985538d5ba1852bed8dcd09639c952f8964fc44a
Muralidharan C <murali.c@ensurity.com>
2021-08-25 00:18:40 +0530

    Add initial files

---
+++ app/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/EnsurityTechnologies/apiconfig"
+)
+
+func main() {
+	key := os.Getenv("API_CONFIG_KEY")
+	if key == "" {
+		if len(os.Args) != 4 {
+			fmt.Printf("Invalid Arguments")
+			return
+		}
+		key = os.Args[3]
+	} else {
+		if len(os.Args) != 3 {
+			fmt.Printf("Invalid Arguments")
+			return
+		}
+	}
+
+	configData, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		fmt.Printf("Failed to open input file %v\n", err)
+		return
+	}
+	err = apiconfig.CreateAPIConfig(os.Args[2], key, configData)
+
+	if err != nil {
+		fmt.Printf("Failed to create API config %v\n", err)
+		return
+	}
+	fmt.Printf("API Config created successfully")
+
+}
